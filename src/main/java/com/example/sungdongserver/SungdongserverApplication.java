ad9cc673a2e6d9f11eac1a546273368f406c3c73
chrhfoq123 <chrhfoq123@naver.com>
2023-08-15 01:17:19 +0900

    settings

---
+++ src/main/java/com/example/sungdongserver/SungdongserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.sungdongserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SungdongserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SungdongserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sungdongserver/SungdongserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sungdongserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SungdongserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
