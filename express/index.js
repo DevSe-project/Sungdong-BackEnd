c7249f020b021d5a8197c0aa28e29f992f536ff0
ptk5758 <76796879+ptk5758@users.noreply.github.com>
2023-11-26 21:39:07 +0900

    express project initialize

--- express/index.js
+++ express/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello World");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = 5050;
+app.get("/", (req, res) => {
+    res.send({ "msg": "Hello SungDong asdaasdasdsd!" });
+});
+app.listen(PORT, () => { console.log(`[SERVER] : http://localhost:${PORT} ON!`); });
--- express/index.ts
+++ express/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello World");
+import express, { Express, Request, Response } from "express"
+const app : Express = express()
+const PORT = 5050;
+app.get("/", (req : Request, res : Response)=>{
+    res.send({"msg" : "Hello SungDong asdaasdasdsd!"});    
+});
+app.listen(PORT, ()=>{ console.log(`[SERVER] : http://localhost:${PORT} ON!`) })
