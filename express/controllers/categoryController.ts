55fda5441e8b782d847eba044c4290dddf479bf9
taehun2123 <pyoneng_@naver.com>
2024-01-23 14:33:45 +0900

    Update : 카테고리 생성 컨트롤러, model 구현완료

--- express/controllers/categoryController.ts
+++ express/controllers/categoryController.ts
@@ -3,77 +3,83 @@ import { Request, Response } from "express"
 import Category from "../models/category.model";
 
 const categoryController = {
-  create : async (req : Request, res : Response) => {
-    if(!req.body){
-        res.status(400).send({
-            message: "내용을 채워주세요!"
-        });
-    };
-    
-    // 각 카테고리의 ID를 저장하기 위한 맵 (함수 외부로 이동)
+  create: async (req: Request, res: Response) => {
     const categoryIds: { [key: string]: number } = {};
-
-    // 카테고리를 생성하는 함수
+  
+    function getNextCategoryId(parentCategory: string | null, lastCategory: any): string {
+      const normalizedParentCategory = parentCategory || '';
+      if (!categoryIds[normalizedParentCategory]) {
+        console.log(lastCategory)
+        const lastCharacter = lastCategory.charAt(lastCategory.length - 1);
+        let num : number;
+        if(lastCategory.length === 1) {
+          num = lastCharacter.charCodeAt(0) - 62
+        } else if(lastCategory.length === 2) {
+          num = lastCharacter.charCodeAt(0) - 95; // Convert letter to numeric value (A=1, B=2, ...)
+        } else {
+          num = parseInt(lastCharacter) + 1
+        }
+        categoryIds[normalizedParentCategory] = num;
+      } else {
+        categoryIds[normalizedParentCategory]++;
+      }
+  
+      const num = categoryIds[normalizedParentCategory];
+  
+      if (normalizedParentCategory.length === 1 && num <= 26) {
+        return `${normalizedParentCategory}${String.fromCharCode(96 + num)}`;
+      } else if (normalizedParentCategory.length === 2 && num <= 100) {
+        return `${normalizedParentCategory}${num}`;
+      } else if(normalizedParentCategory === '' && num <= 26) {
+        return String.fromCharCode(64 + num);
+      }
+      return "카테고리 최대 생성 개수를 초과하였습니다.";
+    }
+  
     function generateCategoryId(parentCategory: string | null): string {
       const normalizedParentCategory = parentCategory || '';
-
-      if (normalizedParentCategory === '') {
-        // 대 카테고리
-        if (!categoryIds[normalizedParentCategory]) {
-          categoryIds[normalizedParentCategory] = 1;
-        } else {
-          categoryIds[normalizedParentCategory]++;
-        }
-        return String.fromCharCode(65 + categoryIds[normalizedParentCategory] - 1);
-      } 
-      else {
-        // 중 또는 소 카테고리
-        if (!categoryIds[normalizedParentCategory]) {
-          categoryIds[normalizedParentCategory] = 1;
-        } else {
-          categoryIds[normalizedParentCategory]++;
-        }
-
-        const num = categoryIds[normalizedParentCategory];
-
-        if (normalizedParentCategory.length === 1) { // 중 카테고리
-          if (num <= 26) {
-            return `${normalizedParentCategory}${String.fromCharCode(65 + num - 1)}`;
-          }
-        } else if (normalizedParentCategory.length === 2) {  // 소 카테고리
-          if (num <= 100) {
-            const parentChars = normalizedParentCategory;
-            const subCategoryPrefix = `${parentChars}${Math.floor((num - 1) / 26) + 1}`;
-            const subCategorySuffix = String.fromCharCode(65 + ((num - 1) % 26));
-            return `${subCategoryPrefix}${subCategorySuffix}`;
-          }
-        }
-
-        // 기본적으로는 대 카테고리와 동일한 로직
-        return String.fromCharCode(65 + num - 1);
+      if (!categoryIds[normalizedParentCategory]) {
+        categoryIds[normalizedParentCategory] = 1;
+      } else {
+        categoryIds[normalizedParentCategory]++;
       }
+  
+      const num = categoryIds[normalizedParentCategory];
+  
+      if (normalizedParentCategory.length == 1) {
+        return `${normalizedParentCategory}${String.fromCharCode(96 + num)}`;
+      } else if (normalizedParentCategory.length == 2) {
+        return `${normalizedParentCategory}${num}`;
+      }
+  
+      return String.fromCharCode(64 + num);
     }
-    // 서버에서 category_id를 생성하고 할당
-
-    const data = req.body.map((item: {
-      name: string; category_id: string; parentsCategory_id: any; 
-    }) => 
-    ({
-      category_id: generateCategoryId(item.parentsCategory_id),
-      parentsCategory_id: item.parentsCategory_id,
-      name: item.name, // 이 부분은 데이터에 실제로 포함된 속성에 따라 조정해야 합니다.
-    }));
-
-    // 데이터베이스에 저장
-    Category.create(data, (err: { message: any; }) =>{
-        // 클라이언트에서 보낸 JSON 데이터를 받음
-        if(err)
-          return res.status(500).send({ message: err.message || "카테고리를 갱신하는 중 서버 오류가 발생했습니다." });
-        else {
-          return res.status(200).json({ message: '성공적으로 생성이 완료되었습니다.', success: true });
-        }
-    })
-  },
+  
+    const parentsCategory = req.body[0].parentsCategory_id;
+    const lastCategory = await Category.lastest(parentsCategory);
+    try {
+        const data = await Promise.all(req.body.map(async (item: { parentsCategory_id: string | null; name: string; category_id: string }) => {
+          if (lastCategory !== null) {
+            const newCategoryId = getNextCategoryId(parentsCategory, lastCategory);
+            return {
+              category_id: newCategoryId,
+              parentsCategory_id: item.parentsCategory_id,
+              name: item.name,
+            }
+          } else {
+            const newCategoryId = generateCategoryId(parentsCategory);
+            return {
+              category_id: newCategoryId,
+              parentsCategory_id: item.parentsCategory_id,
+              name: item.name,
+            };
+          }}))
+        await Category.create(data);
+        res.status(200).json({ message: '성공적으로 생성이 완료되었습니다.', success: true });  
+    } catch (err) {
+      res.status(500).send({ message: "카테고리를 갱신하는 중 서버 오류가 발생했습니다." });
+    }
+  },  
   list : async (req : Request, res : Response) => {
     // 데이터베이스에서 불러오기
     Category.list((err: { message: any; }, data: ResultSetHeader | RowDataPacket | RowDataPacket[] | null) =>{
--- express/models/category.model.ts
+++ express/models/category.model.ts
@@ -6,23 +6,21 @@ const connection = db.getConnection();
 
 class Category {
     // category 튜플 추가
-    static create(newCategory:any, result: (arg0: any, arg1: any) => void) {
+    static create(newCategory:any):any {
       const query = `INSERT INTO category SET ?`;
       const results : any = [];
       for (let i = 0; i < newCategory.length; i++) {
       connection.query(query, newCategory[i], (err: QueryError | null, res:RowDataPacket[]) => {
           if (err) {
             console.log("에러 발생: ", err);
-            result(err, null);
-            return;
+            return err;
           }
           else {
             results.push(res);
             // 마지막 쿼리까지 모두 실행되면 결과를 반환합니다.
             if (results.length === newCategory.length) {
               console.log("새 카테고리가 생성되었습니다: ", ...results);
-              result(null, results);
-              return;
+              return results
             }
           }
         });
@@ -44,32 +42,45 @@ class Category {
           }
       });
     }
-    static lastest(parentCategory: string | null, result: (arg0: any, arg1: any) => void) {
-      let query;
-      if (parentCategory == null) {
-        query = "SELECT category_id FROM category WHERE LENGTH(category_id) = 1 ORDER BY category_id DESC LIMIT 1";
-      } else {
-        query = "SELECT category_id FROM category WHERE parentsCategory_id = ? ORDER BY category_id DESC LIMIT 1";
-      }
-      
-      connection.query(query, parentCategory, (err: QueryError | null, res:RowDataPacket[]) => {
+    static findAllId():any {
+      const query = `SELECT category_id FROM category`;
+      connection.query(query, (err: QueryError | null, res:RowDataPacket[]) => {
           if (err) {
             console.log("에러 발생: ", err);
-            result(err, null);
-            return;
+            return err;
           }
           else {
-            if (res.length > 0) {
-              // 마지막 쿼리까지 모두 실행되면 결과를 반환합니다.
-              console.log("해당 분류의 가장 마지막 카테고리: ", res[0].category_id);
-              result(null, res[0].category_id);
-            } else {
-              // 마지막 글자를 찾지 못했을 때 대체 구문 추가
-              result(null, null);
-            }
+            // 마지막 쿼리까지 모두 실행되면 결과를 반환합니다.
+            console.log("카테고리가 갱신되었습니다: ", res);
+            return res;
           }
       });
     }
+    static async lastest(parentCategory: string | null): Promise<string | null> {
+      return new Promise<string | null>((resolve, reject) => {
+        let query;
+        if (parentCategory == null) {
+          query = "SELECT category_id FROM category WHERE LENGTH(category_id) = 1 ORDER BY category_id DESC LIMIT 1";
+        } else {
+          query = "SELECT category_id FROM category WHERE parentsCategory_id = ? ORDER BY category_id DESC LIMIT 1";
+        }
+    
+        connection.query(query, parentCategory, (err: QueryError | null, res: RowDataPacket[]) => {
+          if (err) {
+            console.log("에러 발생: ", err);
+            reject(err);
+          } else {
+            if (res.length > 0) {
+              console.log("해당 분류의 가장 마지막 카테고리: ", res[0].category_id);
+              resolve(res[0].category_id);
+            } else {
+              console.log("해당 분류의 가장 마지막 카테고리: ", null);
+              resolve(null);
+            }
+          }
+        });
+      });
+    }    
 }
 
 export = Category;
