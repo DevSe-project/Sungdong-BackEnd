261572f439c5d86eb7790e94536344dcd2ff1448
taehun2123 <pyoneng_@naver.com>
2024-01-23 09:12:02 +0900

    Update: 카테고리 마지막부분 찾는 model 추가 (lastest)

--- express/models/category.model.ts
+++ express/models/category.model.ts
@@ -44,6 +44,32 @@ class Category {
           }
       });
     }
+    static lastest(parentCategory: string | null, result: (arg0: any, arg1: any) => void) {
+      let query;
+      if (parentCategory == null) {
+        query = "SELECT category_id FROM category WHERE LENGTH(category_id) = 1 ORDER BY category_id DESC LIMIT 1";
+      } else {
+        query = "SELECT category_id FROM category WHERE parentsCategory_id = ? ORDER BY category_id DESC LIMIT 1";
+      }
+      
+      connection.query(query, parentCategory, (err: QueryError | null, res:RowDataPacket[]) => {
+          if (err) {
+            console.log("에러 발생: ", err);
+            result(err, null);
+            return;
+          }
+          else {
+            if (res.length > 0) {
+              // 마지막 쿼리까지 모두 실행되면 결과를 반환합니다.
+              console.log("해당 분류의 가장 마지막 카테고리: ", res[0].category_id);
+              result(null, res[0].category_id);
+            } else {
+              // 마지막 글자를 찾지 못했을 때 대체 구문 추가
+              result(null, null);
+            }
+          }
+      });
+    }
 }
 
 export = Category;
