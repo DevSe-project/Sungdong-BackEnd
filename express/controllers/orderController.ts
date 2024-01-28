a641db74882f17b2a4e87884fe48351f21b29b05
taehun2123 <pyoneng_@naver.com>
2024-01-28 21:28:40 +0900

    Update: express Session Added, order Controller, model, Router Added

---
+++ express/controllers/orderController.ts
@@ -0,0 +1,123 @@
+import { QueryError, ResultSetHeader, RowDataPacket } from "mysql2";
+import { NextFunction, Request, Response } from "express"
+import Order from "../models/order.model";
+import multer, { Multer } from "multer";
+import path from "path";
+import jwt from 'jsonwebtoken'
+
+const jwtSecret = 'sung_dong'
+
+
+const orderController = { 
+  write: (req: Request, res: Response) => {
+    try {
+      const requestData = req.body;
+      req.session.orderData = requestData;
+      res.status(200).json({ success: true, message: '주문서 작성 데이터를 불러왔습니다..', requestData });
+    } catch (error) {
+      return res.status(500).json({ message: '장바구니에서 주문 작성으로 데이터를 넘기지 못했습니다.' });
+    }
+  }, 
+  read: (req: Request, res: Response) => {
+    try {
+      const orderData = req.session.orderData || {};
+    } catch (error) {
+      return res.status(500).json({ message: '장바구니에서 주문 작성으로 데이터를 넘기지 못했습니다.' });
+    }
+  }, 
+  create: async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.jwt_token;
+      if (!token) {
+        return res.status(401).json({message : "로그인 후 사용 가능합니다."})
+      }
+
+    try {
+      const decoded = jwt.verify(token, jwtSecret);
+      req.user = decoded; // decoded에는 토큰의 내용이 들어 있음
+      const requestData = req.body;
+      const today = new Date();
+      const formattedDate = new Intl.DateTimeFormat('en-Us', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+      }).format(today);
+      const [month, day, year] = formattedDate.split('/');
+      const rearrangedDate = `${year}-${month}-${day}`;
+
+
+  // 중복 체크를 위해 데이터베이스에서 검색
+  Order.findOne([req.user.users_id, requestData.product_id, requestData.category_id, requestData.selectedOption], (err: QueryError | null, data: ResultSetHeader | RowDataPacket | RowDataPacket[] | null) => {
+    if (err) {
+        // 서버 오류가 발생한 경우
+        return res.status(500).send({ message: err || "서버 오류가 발생했습니다." });
+    }
+
+    // 데이터베이스에서 중복된 상품이 검색되면
+    if (data) {
+        return res.status(400).json({ message: "이미 존재하는 상품입니다.", success: false });
+    } else {
+      const newProduct = {
+        product1: {
+          cart_updated: rearrangedDate,
+        },
+        product2: {
+          product_id : requestData.product_id,
+          category_id: requestData.category_id,
+          parentsCategory_id: requestData.parentsCategory_id,
+          cart_price: requestData.product_price,
+          cart_discount: requestData.product_discount,
+          cart_cnt: requestData.cnt,
+          cart_amount: (requestData.product_price - ((requestData.product_price / 100) * requestData.product_discount)) * requestData.cnt,
+          cart_selectedOption : requestData.selectedOption
+        },
+    };
+    Order.create([newProduct, req.user.users_id], (err: { message: any; }, data: ResultSetHeader | RowDataPacket | RowDataPacket[] | null) =>{
+      // 클라이언트에서 보낸 JSON 데이터를 받음
+      if(err)
+        return res.status(500).send({ message: err.message || "상품을 갱신하는 중 서버 오류가 발생했습니다." });
+      else {
+        return res.status(200).json({ message: '성공적으로 상품 생성이 완료 되었습니다.', success: true });
+      }
+    })
+    }})
+  } catch (error) {
+    return res.status(403).json({ message: '회원 인증이 만료되었거나 로그인이 필요합니다.' });
+  }
+  },  
+  list : async (req : Request, res : Response, next: NextFunction) => {
+    const token = req.cookies.jwt_token;
+    if (!token) {
+        return res.status(401).json({msg : "token null"})
+    }
+
+    try {
+      const decoded = jwt.verify(token, jwtSecret);
+      req.user = decoded; // decoded에는 토큰의 내용이 들어 있음
+      const requestData = req.user;
+    // 데이터베이스에서 불러오기
+    Order.list(requestData.users_id, (err: { message: any; }, data: ResultSetHeader | RowDataPacket | RowDataPacket[] | null) =>{
+        // 클라이언트에서 보낸 JSON 데이터를 받음
+        if(err)
+          return res.status(500).send({ message: err.message || "상품을 갱신하는 중 서버 오류가 발생했습니다." });
+        else {
+          return res.status(200).json({ message: '성공적으로 상품 갱신이 완료 되었습니다.', success: true, data });
+        }
+    })
+  } catch (error) {
+    return res.status(403).json({ message: '인증이 만료되었거나 로그인이 필요합니다.' });
+  }
+  },
+  delete : async (req : Request, res : Response) => {
+    const productIds = req.params.ids.split(',').map(Number);
+    Order.deleteByIds(productIds, (err: { message: any; }, data: ResultSetHeader | RowDataPacket | RowDataPacket[] | null) =>{
+        // 클라이언트에서 보낸 JSON 데이터를 받음
+        if(err)
+          return res.status(500).send({ message: err.message || "상품을 갱신하는 중 서버 오류가 발생했습니다." });
+        else {
+          return res.status(200).json({ message: '성공적으로 상품 삭제가 완료 되었습니다.', success: true, data });
+        }
+    })
+  }
+}
+
+export default orderController;
---
+++ express/models/order.model.ts
@@ -0,0 +1,159 @@
+import { QueryError, RowDataPacket, ResultSetHeader, FieldPacket, OkPacket, ProcedureCallPacket, PoolConnection } from 'mysql2';
+import db from '../db';
+
+// getConnection 함수로 connection 객체 얻기
+const connection = db.getConnection();
+const performTransaction = db.performTransaction;
+
+class Order {
+    // category 튜플 추가
+    static create(newProduct:any, result: (arg0: any, arg1: any) => void) {
+        performTransaction((connection: PoolConnection) => {
+            const queries = [
+                "UPDATE cart SET ? WHERE users_id = ?",
+                "INSERT INTO cart_product SET ?, cart_id = (SELECT cart_id FROM cart WHERE users_id = ?)",
+                "UPDATE cart SET cart_totalAmount = (SELECT SUM(cart_amount) AS total FROM cart_product WHERE cart.cart_id = cart_product.cart_id)"
+            ];
+            const results: (OkPacket | RowDataPacket[] | ResultSetHeader[] | RowDataPacket[][] | OkPacket[] | ProcedureCallPacket)[] = [];
+            function executeQuery(queryIndex: number) {
+            if (queryIndex < queries.length) {
+                connection.query(queries[queryIndex], [newProduct[0][`product${queryIndex + 1}`], newProduct[1]], (err, res) => {
+    
+                if (err) {
+                    console.log(`쿼리 실행 중 에러 발생 (인덱스 ${queryIndex}): `, err);
+                    connection.rollback(() => {
+                        result(err, null);
+                        connection.release();
+                        return;
+                    });
+                } else {
+                    results.push(res);
+                    executeQuery(queryIndex + 1);
+                }
+            });
+            } else {
+            connection.commit((commitErr) => {
+            if (commitErr) {
+                console.log('커밋 중 에러 발생: ', commitErr);
+                connection.rollback(() => {
+                    result(commitErr, null);
+                    connection.release();
+                    return;
+                });
+            } else {
+                console.log('트랜잭션 성공적으로 완료: ', results);
+                result(null, results);
+                connection.release();
+            }
+            });
+        }
+        }
+        executeQuery(0);
+    });
+    }
+    static list(user_id: string, result: (arg0: any, arg1: any) => void) {
+      const query = `SELECT * FROM cart JOIN cart_product ON cart.cart_id = cart_product.cart_id JOIN product ON product.product_id = cart_product.product_id WHERE cart.users_id = ?`;
+        connection.query(query, user_id, (err: QueryError | null, res:RowDataPacket[]) => {
+            if (err) {
+                console.log("에러 발생: ", err);
+                result(err, null);
+                connection.releaseConnection;
+                return;
+            }
+            else {
+                // 마지막 쿼리까지 모두 실행되면 결과를 반환합니다.
+                console.log("상품이 갱신되었습니다: ", res);
+                result(null, res);
+                connection.releaseConnection;
+                return;
+            }
+        });
+        }
+        static findOne(data: any[], result: (arg0: any, arg1: any) => void) {
+            const query = `
+            SELECT * 
+            FROM cart_product
+            WHERE cart_product.cart_id = (select cart_id from cart WHERE users_id = ?)
+                AND cart_product.product_id = ?
+                AND cart_product.category_id = ? 
+                AND cart_product.cart_selectedOption = ?`;
+            connection.query(query, [data[0], data[1], data[2], data[3]], (err: QueryError | null, res:RowDataPacket[]) => {
+                try {
+                    if (err) {
+                        console.log("에러 발생: ", err);
+                        result(err, null);
+                    } else {
+                        if (res.length > 0) {
+                            console.log("장바구니에 중복된 상품이 있습니다.: ", res);
+                            result(null, res);
+                        } else {
+                            result(null, null);
+                        }
+                    }
+                } finally {
+                    connection.releaseConnection; // Release the connection in a finally block
+                }
+            });
+        }
+        static edit(newProduct: any, result: (error: any, response: any) => void) {
+        performTransaction((connection: PoolConnection) => {
+    
+        const queries = [
+        "UPDATE product SET ? WHERE product_id = ?",
+        "UPDATE product_option SET ? WHERE product_id = ?",
+        ];
+    
+        const results: (OkPacket | RowDataPacket[] | ResultSetHeader[] | RowDataPacket[][] | OkPacket[] | ProcedureCallPacket)[] = [];
+    
+        function executeQuery(queryIndex: number) {
+        if (queryIndex < queries.length) {
+            connection.query(queries[queryIndex], [newProduct[`product${queryIndex + 1}`], newProduct[`product${queryIndex + 1}`].product_id], (err, res) => {
+            if (err) {
+                console.log(`쿼리 실행 중 에러 발생 (인덱스 ${queryIndex}): `, err);
+                connection.rollback(() => {
+                result(err, null);
+                connection.release();
+                });
+            } else {
+                results.push(res);
+                executeQuery(queryIndex + 1);
+            }
+            });
+        } else {
+            connection.commit((commitErr) => {
+            if (commitErr) {
+                console.log('커밋 중 에러 발생: ', commitErr);
+                connection.rollback(() => {
+                result(commitErr, null);
+                connection.release();
+                });
+            } else {
+                console.log('트랜잭션 성공적으로 완료: ', results);
+                result(null, results);
+                connection.release();
+            }
+            });
+        }
+        }
+        executeQuery(0);
+    });
+    }
+    static deleteByIds(product: number[], result: (error: any, response: any) => void) {
+    const query = "DELETE FROM cart_product WHERE cart_product_id IN (?)"
+    console.log(query)
+    console.log(product)
+            connection.query(query, [product], (err, res) => {
+            if (err) {
+                console.log(`쿼리 실행 중 에러 발생: `, err);
+                result(err, null);
+                connection.releaseConnection;
+            } else {
+                console.log('성공적으로 삭제 완료: ', res);
+                result(null, res);
+                connection.releaseConnection;
+            }
+        })
+    }           
+}   
+
+export = Order;
