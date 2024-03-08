33ef27449bb620c11ad996cc1237ed4331644edd
jsum01 <djadjawltjr@gmail.com>
2024-03-08 18:28:29 +0900

    Update Delivery Filtering API

--- express/controllers/deliveryController.ts
+++ express/controllers/deliveryController.ts
@@ -5,16 +5,16 @@ import Delivery from "../models/delivery.model";
 const deliveryController = {
   // 모든 배송 데이터 조회
   deliveryAll: async (req: Request, res: Response) => {
-    const currentPage = parseInt(req.query.page as string, 10) || 1; // 페이지 번호 쿼리 파라미터를 읽어옴
-    const itemsPerPage = parseInt(req.query.pagePosts as string, 10) || 10; // 페이지 당 아이템 개수 쿼리 파라미터를 읽어옴
+    const currentPage = parseInt(req.query.page as string, 10) || 1; // 페이지 번호 쿼리 파라미터를 읽습니다.
+    const itemsPerPage = parseInt(req.query.pagePosts as string, 10) || 10; // 페이지 당 아이템 개수 쿼리 파라미터를 읽습니다.
 
     // 데이터베이스에서 불러오기
     Delivery.getDeliveries(currentPage, itemsPerPage, (err: { message: any; }, data: ResultSetHeader | RowDataPacket | RowDataPacket[] | null) => {
       // 클라이언트에서 보낸 JSON 데이터를 받음
       if (err)
-        return res.status(500).send({ message: err.message || "상품을 갱신하는 중 서버 오류가 발생했 습니다." });
+        return res.status(500).send({ message: err.message || "데이터를 갱신하는 중 서버 오류가 발생했 습니다." });
       else {
-        return res.status(200).json({ message: '성공적으로 상품 갱신이 완료 되었습니다.', success: true, data });
+        return res.status(200).json({ message: '데이터가 성공적으로 갱신이 완료 되었습니다.', success: true, data });
       }
     })
   },
@@ -77,9 +77,34 @@ const deliveryController = {
     Delivery.deleteByIds(orderIds, (err: { message: any; }) => {
       // 클라이언트에서 보낸 JSON 데이터를 받음
       if (err)
-        return res.status(500).send({ message: err.message || "상품을 갱신하는 중 서버 오류가 발생했습니다." });
+        return res.status(500).send({ message: err.message || "데이터를 갱신하는 중 서버 오류가 발생했습니다." });
       else {
-        return res.status(200).json({ message: '성공적으로 상품 삭제가 완료 되었습니다.', success: true });
+        return res.status(200).json({ message: '성공적으로 삭제가 완료 되었습니다.', success: true });
+      }
+    })
+  },
+
+  // 필터링
+  filter: async (req: Request, res: Response) => {
+    const checkboxState = req.body.checkboxState; // 사용하기 쉽게 변수에 담기
+    const filteredStateKeys = Object.keys(checkboxState).filter(key => checkboxState[key] === true); // 값이 true인 키만 추출
+    const currentPage = parseInt(req.query.page as string, 10) || 1; // 조회할 페이지 번호
+    const itemsPerPage = parseInt(req.query.pagePosts as string, 10) || 10; // 조회할 페이지의 아이템 개수
+    const newFilter = {
+      orderState: filteredStateKeys || "",
+      startDate: req.body.date.start || "",
+      endDate: req.body.date.end || ""
+    }
+
+    console.log(newFilter);
+
+    Delivery.filteredData(newFilter, currentPage, itemsPerPage, (err: { message: any; }, data: ResultSetHeader | RowDataPacket | RowDataPacket[] | null) => {
+      // 클라이언트에서 보낸 JSON 데이터를 받습니다.
+      if (err)
+        return res.status(500).send({ message: err.message || "데이터를 갱신하는 중 서버 오류가 발생했습니다." });
+      else {
+        return res.status(200).json({ message: '필터링이 완료 되었습니다.', success: true, data });
+        
       }
     })
   }
--- express/models/delivery.model.ts
+++ express/models/delivery.model.ts
@@ -1,5 +1,6 @@
 import { QueryError, ResultSetHeader, RowDataPacket } from "mysql2";
 import db from "../db";
+import { Query } from "mysql2/typings/mysql/lib/protocol/sequences/Query";
 
 class Delivery {
 
@@ -133,6 +134,117 @@ class Delivery {
     });
   }
 
+
+  /**
+   * 배송관리 필터 컴포넌트에서 요청한 필터링된 정보를 반환합니다.
+   * @param state Client에서 필터링 데이터로 체크된 배송 상태를 담습니다.
+   * @param date Client에서 필터링 Range로 지정한 날짜를 담습니다.
+   * @param result 
+   */
+  static filteredData(newFilter: any, currentPage: number, itemsPerPage: number, result: (error: any, data: any) => void) {
+    const offset = (currentPage - 1) * itemsPerPage; //
+    const limit = itemsPerPage;
+
+    // 조인 쿼리
+    let joinedQuery = `
+    SELECT 
+      d.order_id, 
+      o.orderState,
+      d.delivery_selectedCor,
+      d.delivery_num, 
+      DATE_FORMAT(o.order_date, '%Y-%m-%d') as order_date, 
+      op.product_id, 
+      p.product_title,
+      op.selectedOption, 
+      p.product_price, 
+      ROUND((p.product_price * (1 - (p.product_discount * 0.01)))) as discountPrice 
+    FROM 
+      delivery d
+    JOIN 
+      \`order\` o ON d.order_id = o.order_id
+    JOIN 
+      order_product op ON o.order_id = op.order_id
+    JOIN 
+      product p ON op.product_id = p.product_id
+    WHERE 1=1`; // : always true condition
+
+    /** 배송 상태 필터링 쿼리 */
+    const stateFitlerQuery = newFilter.orderState ? `AND orderState IN (?)` : ``;
+
+    /** 날짜 필터링 쿼리 */
+    const dateFilterQuery = newFilter.startDate && newFilter.endDate ? `AND o.order_date BETWEEN DATE_FORMAT(?, "%Y-%m-%d") AND DATE_FORMAT(?, "%Y-%m-%d")` : ``;
+
+    /** ORDER BY: 정렬 */
+    const orderBy = `ORDER BY o.order_date DESC, o.orderState DESC`;
+
+    /** LIMIT: 페이징 */
+    const paging = `LIMIT ${offset}, ${limit}`;
+
+    /** 완성 쿼리 */
+    const query = `${joinedQuery} ${stateFitlerQuery} ${dateFilterQuery} ${orderBy} ${paging}`
+
+    /** 쿼리 파라미터 */
+    const queryParams = [
+      newFilter.orderState ? newFilter.orderState : null,
+      newFilter.startDate ? newFilter.startDate : null,
+      newFilter.endDate ? newFilter.endDate : null
+    ]
+    console.log(`쿼리 파라미터: ${queryParams}`);
+
+    const mysql = require('mysql');
+    // 쿼리와 파라미터를 합친 문자열 생성
+    const fullQuery = mysql.format(query, queryParams);
+
+    // 쿼리와 파라미터를 합친 문자열 출력
+    console.log(`Full query: ${fullQuery}`);
+
+    /** 전체 데이터 확인 */
+    const countQuery = `
+      SELECT 
+        COUNT(*) as totalRows 
+      FROM 
+        delivery d
+      JOIN 
+        \`order\` o ON d.order_id = o.order_id
+      JOIN 
+        order_product op ON o.order_id = op.order_id
+      JOIN 
+        product p ON op.product_id = p.product_id
+    `;
+
+
+    // 전체 데이터 크기 확인을 위한 쿼리
+    this.connection.query(countQuery, (countErr, countResult: any) => {
+      if (countErr) {
+        result(countErr, null);
+        this.connection.releaseConnection;
+        return;
+      }
+      const totalRows = countResult[0].totalRows;
+
+      this.connection.query(query, queryParams, (err: QueryError | null, res: RowDataPacket[]) => {
+        if (err) {
+          console.log("에러 발생: ", err);
+          result(err, null);
+          this.connection.releaseConnection;
+          return;
+        } else {
+          const totalPages = Math.ceil(totalRows / itemsPerPage);
+
+          const responseData = {
+            data: res,
+            currentPage: currentPage,
+            totalPages: totalPages,
+          }
+          // 마지막 쿼리까지 모두 실행되면 결과를 반환합니다.
+          console.log("상품이 갱신되었습니다: ", responseData);
+          result(null, responseData);
+          this.connection.releaseConnection;
+          return;
+        }
+      });
+    });
+  };
 }
 
 export default Delivery;
