e6246aef3c2d9d66801ab315c0096bd971ea4c33
taehun2123 <pyoneng_@naver.com>
2024-04-01 13:27:19 +0900

    Update: 상품 대량등록 :: 엑셀파일로 업로딩하는 구문 구현

--- express/routes/product.ts
+++ express/routes/product.ts
@@ -9,6 +9,7 @@ productRouter.post("/relate", productController.relate);
 productRouter.post("/filter", productController.filter);
 productRouter.post("/create", productController.create);
 productRouter.post("/upload", productController.upload);
+productRouter.post("/upload/excel", productController.uploadExcel);
 productRouter.patch("/edit", productController.edit);
 productRouter.put("/categoryEdit", productController.categoryEdit);
 productRouter.put("/supplyLow", productController.supplyLow);
